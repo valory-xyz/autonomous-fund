1838dbb913796f50fbd912c35c543837ccf0e142
Ardian <ardianabazi09@gmail.com>
2022-09-05 11:14:55 +0200

    chore: synced utils with agent-academy-1

--- sync.sh
+++ sync.sh
@@ -36,3 +36,7 @@ cp -r ../open-autonomy/packages/valory/skills/transaction_settlement_abci packag
 cp -r ../open-autonomy/scripts/__init__.py scripts/
 cp -r ../open-autonomy/scripts/check_copyright.py scripts/
 cp -r ../open-autonomy/scripts/check_packages.py scripts/
+cp -r ../agent-academy-1/.pylintrc .
+cp -r ../agent-academy-1/pytest.ini .
+cp -r ../agent-academy-1/setup.cfg .
+cp -r ../agent-academy-1/tox.ini .
