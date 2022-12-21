02d735d92b24d04d0747570059897ce2ad27ac2c
Ardian <ardianabazi09@gmail.com>
2022-12-21 20:18:20 +0100

    feat: add `AllowListUpdateBehaviour` base logic [no ci]

--- packages/balancer/skills/liquidity_provision_abci/behaviours.py
+++ packages/balancer/skills/liquidity_provision_abci/behaviours.py
@@ -19,8 +19,10 @@
 
 """This package contains round behaviours of LiquidityProvisionAbciApp."""
 
-from typing import Generator, Set, Type, cast
+from typing import Generator, Set, Type, cast, Optional, List, Dict
 
+from packages.balancer.contracts.managed_pool.contract import ManagedPoolContract
+from packages.valory.protocols.contract_api import ContractApiMessage
 from packages.valory.skills.abstract_round_abci.base import AbstractRound
 from packages.valory.skills.abstract_round_abci.behaviours import (
     AbstractRoundBehaviour,
@@ -37,6 +39,10 @@ from packages.balancer.skills.liquidity_provision_abci.rounds import (
     AllowListUpdatePayload,
 )
 
+# setting the safe gas to 0 means that all available gas will be used
+# which is what we want in most cases
+# more info here: https://safe-docs.dev.gnosisdev.com/safe/docs/contracts_tx_execution/
+SAFE_GAS = 0
 
 class LiquidityProvisionBaseBehaviour(BaseBehaviour):
     """Base behaviour for the common apps' skill."""
@@ -57,13 +63,12 @@ class AllowListUpdateBehaviour(LiquidityProvisionBaseBehaviour):
 
     matching_round: Type[AbstractRound] = AllowListUpdateRound
 
-    # TODO: implement logic required to set payload content (e.g. synchronized_data)
     def async_act(self) -> Generator:
         """Do the act, supporting asynchronous execution."""
 
         with self.context.benchmark_tool.measure(self.behaviour_id).local():
-            sender = self.context.agent_address
-            payload = AllowListUpdatePayload(sender=sender, content=...)
+            update_payload = yield from self.get_allow_list_update()
+            payload = AllowListUpdatePayload(sender=self.context.agent_address, allow_list_update=update_payload)
 
         with self.context.benchmark_tool.measure(self.behaviour_id).consensus():
             yield from self.send_a2a_transaction(payload)
@@ -71,7 +76,101 @@ class AllowListUpdateBehaviour(LiquidityProvisionBaseBehaviour):
 
         self.set_done()
 
+    def get_allow_list_update(self) -> Generator[None, None, str]:
+        """
+        Checks whether the pool is running with the correct LP allowlist params, if not it prepares a (multisend) tx to update it.
 
+        There are two parameters we need to take into consideration.
+        1.  Whether an allowlist for LPs should be enforced.
+        2.  Whether the allowlist is the same as the local one.
+
+        Note that if the allowlist is not being enforced, any address will be allowed to provide liqudity, regardless of
+        whether they are on the allowlist or not.
+
+        :return: the payload, tx data if an update is required, a "special" payload if otherwise.
+        """
+        is_allowlist_enforced = yield from self._is_allowlist_enforced()
+        if is_allowlist_enforced is None:
+            # an error was encountered if this is None
+            return AllowListUpdateRound.NoUpdatePayloads.ERROR_PAYLOAD.value
+
+        current_allowlist = yield from self._get_current_allowlist()
+        if current_allowlist is None:
+            # an error was encountered if the allowlist is None
+            return AllowListUpdateRound.NoUpdatePayloads.ERROR_PAYLOAD.value
+
+        required_updates = yield from self._get_required_update_txs(current_allowlist, is_allowlist_enforced)
+        if required_updates is None:
+            # an error was encountered while checking and preparing txs for the required updates
+            return AllowListUpdateRound.NoUpdatePayloads.ERROR_PAYLOAD.value
+
+        if len(required_updates) == 0:
+            # no updates are required
+            self.context.logger.info("No updates to the allowlist are required.")
+            return AllowListUpdateRound.NoUpdatePayloads.NO_UPDATE_PAYLOAD.value
+
+
+
+    def _is_allowlist_enforced(self) -> Generator[None, None, Optional[bool]]:
+        """Returns whether the pool is configured to enforce an allowlist."""
+        response = yield from self.get_contract_api_response(
+            performative=ContractApiMessage.Performative.GET_STATE,  # type: ignore
+            contract_id=str(ManagedPoolContract.contract_id),
+            contract_callable="get_must_allowlist_lps",
+            contract_address=self.params.managed_pool_address,
+        )
+
+        if response.performative != ContractApiMessage.Performative.STATE:
+            self.context.logger.error(
+                f"Couldn't whether the pool is configured to enforce an allowlist from IManagedPool.get_must_allowlist_lps. "
+                f"Expected response performative {ContractApiMessage.Performative.STATE.value}, "  # type: ignore
+                f"received {response.performative.value}."
+            )
+            return None
+
+        params = cast(Optional[bool], response.state.body.get("is_enforced", None))
+        return params
+
+    def _get_current_allowlist(self) -> Generator[None, None, Optional[List[str]]]:
+        """Returns the current allow-list the pool has."""
+        response = yield from self.get_contract_api_response(
+            performative=ContractApiMessage.Performative.GET_STATE,  # type: ignore
+            contract_id=str(ManagedPoolContract.contract_id),
+            contract_callable="get_allowlist",
+            contract_address=self.params.managed_pool_address,
+        )
+
+        if response.performative != ContractApiMessage.Performative.STATE:
+            self.context.logger.error(
+                f"Couldn't get weight update params from IManagedPool.get_allowlist. "
+                f"Expected response performative {ContractApiMessage.Performative.STATE.value}, "  # type: ignore
+                f"received {response.performative.value}."
+            )
+            return None
+
+        params = cast(Optional[List[str]], response.state.body.get("allowlist", None))
+        return params
+
+    def _get_required_update_txs(self, current_allowlist: List[str], is_allowlist_currently_enforced: bool) -> Generator[None, None, Optional[List[Dict]]]:
+        """Returns the required update txs to be made."""
+        # TODO: add docstring about multisend
+
+        if self.params.enforce_allowlist != is_allowlist_currently_enforced:
+            ...
+
+        current_set, required_set = set(current_allowlist), set(self.params.allowed_lp_addresses)
+        members_to_be_removed = current_set - required_set
+        members_to_be_added = required_set - current_set
+
+        for member in members_to_be_removed:
+            # TODO: convert set to list and sort it
+            ...
+
+        for member in members_to_be_added:
+            # TODO: convert set to list and sort it
+            ...
+        return None
+        yield
 class LiquidityProvisionRoundBehaviour(AbstractRoundBehaviour):
     """LiquidityProvisionRoundBehaviour"""
 
